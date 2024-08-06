b37580cb39e2323eb465de89a2e988b98201669d
nickolaykondratyev <kondratyev.nickolay@gmail.com>
2024-08-05 22:12:56 -0700

    [AI-GEN-MSG] Update keymap to replace ADJUST with RAISE in lily58 keyboard layout

--- keyboards/lily58/keymaps/nkondrat/keymap.c
+++ keyboards/lily58/keymaps/nkondrat/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |LShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LGUI | LAlt | ADJUST| /Space  /       \Enter \  |LOWER | LAlt | LGUI |
+ *                   | LGUI | LAlt | _RAISE| /Space  /       \Enter \  |LOWER | LAlt | LGUI |
  *                   |      |      |       |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
   KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_LCTL,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT,
-                        KC_LGUI, KC_LALT, MO(_ADJUST), KC_SPC, KC_ENT, MO(_LOWER), KC_LALT, KC_LGUI
+                        KC_LGUI, KC_LALT, MO(_RAISE), KC_SPC, KC_ENT, MO(_LOWER), KC_LALT, KC_LGUI
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
