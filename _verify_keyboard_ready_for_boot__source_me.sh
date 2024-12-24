d151e4a85d925654e2d54b669789c8eac17b4dbf
nickolaykondratyev <kondratyev.nickolay@gmail.com>
2024-12-24 10:51:11 -0800

    [AI-GEN-MSG] Add log message for successful detection of RPI-RP2 in boot verification script

--- _verify_keyboard_ready_for_boot__source_me.sh
+++ _verify_keyboard_ready_for_boot__source_me.sh
@@ -1,4 +1,6 @@
 if ! diskutil list | grep --quiet RPI-RP2; then
     echo "Did not find RPI-RP2 in 'diskutil list'. Exiting."
     exit 1
+else
+    echo.log "Found RPI-RP2 in 'diskutil list'. Deeming as ready for boot. Continuing."
 fi
