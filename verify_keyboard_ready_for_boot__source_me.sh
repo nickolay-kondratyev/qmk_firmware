d47897338d5e2c5dd6a4967f5e48390866e1bbe2
Nickolay Kondratyev <nickolay.kondratyev@gmail.com>
2025-06-18 11:44:55 -0600

    [2025-06-18T10-44-55PDT] Modified files: verify_keyboard_ready_for_boot__source_me.sh

--- verify_keyboard_ready_for_boot__source_me.sh
+++ verify_keyboard_ready_for_boot__source_me.sh
@@ -16,12 +16,30 @@ _mac_verify_keyboard_ready_for_boot() {
   fi
 }
 
+_linux_verify_keyboard_ready_for_boot() {
+  if ! lsblk | grep --quiet RPI-RP2; then
+    echo.yellow "Did not find RPI-RP2 in 'lsblk'. Exiting."
+    exit 1
+  else
+    echo.log "Found RPI-RP2 in 'lsblk'. Deeming as ready for boot. Continuing."
+  fi
+
+  # Ensure the RP2040 is mounted and ready
+  local bootloader_path="/media/$(whoami)/RPI-RP2"
+  if [[ ! -d "$bootloader_path" ]]; then
+    echo.yellow "Error: RP2040 bootloader not detected at $bootloader_path. Please enter bootloader mode."
+    exit 1
+  else
+    echo.log "RP2040 bootloader detected at $bootloader_path. Continuing."
+  fi
+}
+
 main() {
   if is_mac; then
     _mac_verify_keyboard_ready_for_boot
   elif is_linux; then
     # Linux with PopOS/Ubuntu
-    
+    _linux_verify_keyboard_ready_for_boot
   else
     throw "Unsupported OS uname=[$(uname)]"
   fi
