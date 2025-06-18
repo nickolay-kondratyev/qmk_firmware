488b4034e8992b3dc3b888b390695a738936a644
Nickolay Kondratyev <nickolay.kondratyev@gmail.com>
2025-06-18 11:43:35 -0600

    [2025-06-18T10-43-35PDT] Modified files: verify_keyboard_ready_for_boot__source_me.sh

--- verify_keyboard_ready_for_boot__source_me.sh
+++ verify_keyboard_ready_for_boot__source_me.sh
@@ -1,21 +1,30 @@
+_mac_verify_keyboard_ready_for_boot() {
+  if ! diskutil list | grep --quiet RPI-RP2; then
+    echo.yellow "Did not find RPI-RP2 in 'diskutil list'. Exiting."
+    exit 1
+  else
+    echo.log "Found RPI-RP2 in 'diskutil list'. Deeming as ready for boot. Continuing."
+  fi
 
+  # Ensure the RP2040 is mounted and ready
+  local bootloader_path="/Volumes/RPI-RP2"
+  if [[ ! -d "$bootloader_path" ]]; then
+      echo.yellow "Error: RP2040 bootloader not detected at $bootloader_path. Please enter bootloader mode."
+      exit 1
+  else
+      echo.log "RP2040 bootloader detected at $bootloader_path. Continuing."
+  fi
+}
 
 main() {
-    if ! diskutil list | grep --quiet RPI-RP2; then
-      echo.yellow "Did not find RPI-RP2 in 'diskutil list'. Exiting."
-      exit 1
-    else
-      echo.log "Found RPI-RP2 in 'diskutil list'. Deeming as ready for boot. Continuing."
-    fi
-
-    # Ensure the RP2040 is mounted and ready
-    local bootloader_path="/Volumes/RPI-RP2"
-    if [[ ! -d "$bootloader_path" ]]; then
-        echo.yellow "Error: RP2040 bootloader not detected at $bootloader_path. Please enter bootloader mode."
-        exit 1
-    else
-        echo.log "RP2040 bootloader detected at $bootloader_path. Continuing."
-    fi
+  if is_mac; then
+    _mac_verify_keyboard_ready_for_boot
+  elif is_linux; then
+    # Linux with PopOS/Ubuntu
+    
+  else
+    throw "Unsupported OS uname=[$(uname)]"
+  fi
 }
 
 main "${@}" || exit 1
