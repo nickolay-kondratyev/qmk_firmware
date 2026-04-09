e1cc4b6e81484a4cc32fe38e97263c0605e96b63
CC_opus-v4.6_WITH-nickolaykondratyev <nickolay.kondratyev@gmail.com>
2026-04-09 09:12:00 -0600

    Modified file: keymap.c

--- keyboards/lily58/keymaps/nkondrat/keymap.c
+++ keyboards/lily58/keymaps/nkondrat/keymap.c
@@ -533,7 +533,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                  return false;
 
              case KT_IMP:
-                  send_string("import kotlin.io.*" SS_TAP(X_ENT) "import java.util.*" SS_TAP(X_ENT) "import kotlin.math.*");
+                  send_string("import kotlin.io.*" SS_TAP(X_ENT) "import java.util.*" SS_TAP(X_ENT) "import kotlin.math.*" SS_TAP(X_ENT) "import kotlinx.serialization.json.*");
                   return false;
              case KTMAIN:
                   send_string("fun main(args: Array<String>) {" SS_TAP(X_ENT) "println(\"DONE\")");
