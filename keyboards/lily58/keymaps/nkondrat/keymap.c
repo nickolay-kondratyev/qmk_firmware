82a0a6d11e4b0e393582f6636108d9998f688b4e
CC_opus-v4.6_WITH-nickolaykondratyev <nickolay.kondratyev@gmail.com>
2026-04-14 16:41:21 -0600

    Modified file: keymap.c

--- keyboards/lily58/keymaps/nkondrat/keymap.c
+++ keyboards/lily58/keymaps/nkondrat/keymap.c
@@ -170,6 +170,7 @@ enum custom_keycodes {
     CODE_BLOCK,
 
     KTCLASS,
+    KTINTER,
     KT_EXC,
     KTPRNT,
     KT_IMP,
@@ -290,7 +291,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |  ~   |  `   |  ~   |C3MO_3|C3MO_4|C3MO_5|                    |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |C3MO_Q|KTTEST|KTFUNC|CP_F4 |CP_GTS|                    | F12  |  '   |KT_IMP|KTPRNT|  Up  | PgUp |
+ * |      |KTINTER|KTCLASS|KTFUNC|CP_F4 |CP_GTS|                    | F12  |  '   |KT_IMP|KTPRNT|  Up  | PgUp |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |CP_F1 |C3MO_S|CP_DEF|CP_FREF|CP_GTL|-------.    ,-------|C3MO_H| Left |Right | Down |      |PgDwn |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
@@ -302,7 +303,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LH_L1] = LAYOUT(
   KC_TILD, KC_GRV,  KC_TILD,  C3MO_3,  C3MO_4,  C3MO_5,                     KC_F6,   KC_F7,    KC_F8,   KC_F9, KC_F10,  KC_F11,
-  KC_TRNS,  C3MO_Q,  KTCLASS,  KTFUNC,  CP_F4,  CP_GTS,                   KC_F12,  KC_QUOT, KT_IMP, KTPRNT, KC_UP,   KC_PGUP,
+  KC_TRNS,  KTINTER,  KTCLASS,  KTFUNC,  CP_F4,  CP_GTS,                   KC_F12,  KC_QUOT, KT_IMP, KTPRNT, KC_UP,   KC_PGUP,
   KC_TRNS,  CP_F1,  C3MO_S,  CP_DEF,  CP_FREF,  CP_GTL,                   C3MO_H, KC_LEFT, KC_RGHT, KC_DOWN, KC_TRNS,  KC_PGDN,
   KC_TRNS,  C3MO_Z,  KT_EXC,  KTCLASS, C3MO_V,  CPPROB, KC_TRNS, KC_TRNS, KC_DOWN, KC_HOME, KC_END, C3MO_GT, KC_TRNS, KC_TRNS,
                          KC_LGUI, KC_LALT, KC_TRNS,  MO(_LH_L2),      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
@@ -530,6 +531,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                  unregister_code(KC_LSFT);
                  unregister_code(KC_LCTL);
                  return false;
+             case KTINTER:
+                  send_string("interface X{}" SS_TAP(X_LEFT) SS_TAP(X_LEFT) SS_TAP(X_BSPC));
+                             return false;
              case KTCLASS:
                 send_string("class X{}" SS_TAP(X_LEFT) SS_TAP(X_LEFT) SS_TAP(X_BSPC));
                 return false;
