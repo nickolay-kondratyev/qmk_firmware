924fafe8b51b3500eb1f402041701d64f12172ad
CC_opus-v4.6_WITH-nickolaykondratyev <nickolay.kondratyev@gmail.com>
2026-04-21 12:42:14 -0600

    Modified file: keymap.c

--- keyboards/lily58/keymaps/nkondrat/keymap.c
+++ keyboards/lily58/keymaps/nkondrat/keymap.c
@@ -546,7 +546,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                  return false;
 
              case KT_IMP:
-                  send_string("import kotlin.io.*" SS_TAP(X_ENT) "import java.util.*" SS_TAP(X_ENT) "import kotlin.math.*" SS_TAP(X_ENT) "import kotlin.time.*" SS_TAP(X_ENT) "import kotlinx.serialization.json.*");
+                  send_string("this.");
                   return false;
              case KTFUNC:
                   send_string("void f() {" SS_TAP(X_ENT) "throw new RuntimeException(\"Not Implemented\");" SS_TAP(X_UP) SS_TAP(X_LEFT) SS_TAP(X_LEFT) SS_TAP(X_LEFT) SS_TAP(X_LEFT) SS_TAP(X_BSPC) SS_TAP(X_BSPC) SS_TAP(X_BSPC) SS_TAP(X_BSPC) SS_TAP(X_BSPC) SS_TAP(X_BSPC));
